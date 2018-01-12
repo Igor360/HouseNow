31506b943cd3e93fdfb94c98c1065f6468c2fce9
Igor360 <igor.dawidencko@gmaiul.com>
2018-01-12 21:34:24 +0200

    - config canges

--- src/main/java/com/igordavidenko/HouseNow/repo/UsersRepository.java
+++ src/main/java/com/igordavidenko/HouseNow/repo/UsersRepository.java
@@ -13,9 +13,9 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
 			public Users findUserByUsername(String username);
 			public Users findUserByEmail(String email);
 				
-			@Modifying
-			@Query("UPDATE Users u SET u.username = :username,  u.email = :email, u.firstName = :firstName, u.lastName = :lastName, u.password = :password WHERE u.id = :id")
-			public int updateUser(@Param("id") Long integer, @Param("username")String username, @Param("email") String email, @Param("firstName") String firstName, @Param("lastName") String lastName, @Param("password") String password);
+					@Modifying
+					@Query("UPDATE Users u SET u.username = :username,  u.email = :email, u.firstName = :firstName, u.lastName = :lastName, u.password = :password WHERE u.id = :id")
+					public int updateUser(@Param("id") Long integer, @Param("username")String username, @Param("email") String email, @Param("firstName") String firstName, @Param("lastName") String lastName, @Param("password") String password);
 			
 			@Modifying
 			@Query("UPDATE Users u SET u.username = :username,  u.email = :email, u.firstName = :firstName, u.lastName = :lastName WHERE u.id = :id")
